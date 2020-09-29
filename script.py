803531b7a67a3296b7a94ed86a53c550ff0598ca
ROKBASSS <anton.fomi@ya.ru>
2020-09-29 16:56:41 +0300

    first commit

---
+++ script.py
@@ -0,0 +1,50 @@
+import json
+import requests
+import time
+import platform
+import sys
+
+# payload = {'key1': 'value1', 'key2': 'value2'}  
+# r = requests.get('https://httpbin.org/get', params=payload)  
+# http://httpbin.org/get?key2=value2&key1=value1
+
+def request_site(ip:str):
+    ok = False
+    r  = requests.get(ip)
+    if(r.status_code == requests.codes.ok): ok = True
+    else: ok = False
+    return ok
+    
+
+def main_loop():
+    sites = load_data()
+    while True:
+        try:
+            for i in sites:
+                ok = request_site(i['ip'])
+                i['status'] = ok
+                # print(ok)
+            with open("sites.json", "w") as c_data:
+                json.dump(sites, c_data)
+            time.sleep(60)
+        except KeyboardInterrupt:
+            # print("You stopped me! Fool!")
+            sys.exit(0)
+
+def load_data():
+    with open("sites.json") as c_data:
+        data = c_data.read()
+        sites = json.loads(data)
+        return sites
+
+def main(argv):
+    platforma = platform.system()
+    if platforma == "Linux":
+        import daemon
+        with daemon.DaemonContext():
+            main_loop()
+    elif platforma == "Windows":
+        main_loop()
+
+if __name__ == '__main__':
+    main(sys.argv)
